18599daaeac15f8b2188100cb2d697b31922e180
Eugene Koontz <ekoontz@Mac-Pro-Desktop.local>
2017-04-16 18:19:43 -0700

    more feedback during image building

--- build-osx.sh
+++ build-osx.sh
@@ -6,12 +6,18 @@ dd if=/dev/zero bs=1024 count=1440 > disk_images/mikeos.flp
 /usr/local/Cellar/dosfstools/4.1/sbin/mkfs.fat disk_images/mikeos.flp
 cp disk_images/mikeos.flp disk_images/mikeos.dmg
 
+echo ">>> Compile bootload.bin..."
+
 nasm -O0 -w+orphan-labels -f bin -o source/bootload/bootload.bin source/bootload/bootload.asm || exit
+
+echo ">>> Compile kernel..."
 
 cd source
 rm kernel.bin
 nasm -O0 -w+orphan-labels -f bin -o kernel.bin kernel.asm || exit
 cd ..
+
+echo ">>> Compile user programs..."
 
 cd programs
 rm *.bin
@@ -21,6 +27,8 @@ do
 done
 
 cd ..
+
+echo ">>> Creating iso.."
 
 dd conv=notrunc if=source/bootload/bootload.bin of=disk_images/mikeos.dmg
 dev=`hdid -nobrowse -nomount disk_images/mikeos.dmg`
@@ -32,4 +40,5 @@ rm -rf tmp-loop
 rm -f disk_images/mikeos.iso
 mkisofs -quiet -V 'MIKEOS' -input-charset iso8859-1 -o disk_images/mikeos.iso -b mikeos.dmg disk_images/ 
 ls -lt disk_images
-echo  qemu-system-i386 -cdrom disk_images/mikeos.iso
+echo "Now you can do:"
+echo "  qemu-system-i386 -cdrom disk_images/mikeos.iso"
